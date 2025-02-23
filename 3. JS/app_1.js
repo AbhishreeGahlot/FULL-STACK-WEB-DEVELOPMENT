4a5846c53b33ff936d3c1274d2dccc3a788ddb29
Abhishree Gahlot <117459964+AbhishreeGahlot@users.noreply.github.com>
2025-02-23 10:16:58 +0530

    Add files via upload

---
+++ 3. JS/app_1.js
@@ -0,0 +1,59 @@
+console.log("HELLO THIS IS FIRST LINE OF JAVASCRIPT");
+
+// practice q1
+// light="red";
+
+// if(light=='green')
+// {
+//     console.log("YOU CAN GOOOO");
+// }
+// else if (light=='yellow')
+// {
+//     console.log("slow down ");
+// }
+// else if(light='red')
+// {
+//     console.log("stop");
+// }
+
+
+// practice q 2
+// size='xl' ;
+// if(size=='S')
+// {
+//     console.log("price 50 ");
+// }
+
+// else if(size=='M')
+// {
+//     console.log("price 100 ");
+// }
+
+// else if(size=='L')
+// {
+//     console.log("price 200");
+// }
+
+// else
+// {
+//     console.log("price 250");
+// }
+
+// str="apple"
+// if(str[0]==='a' && str.length>3 )
+// {
+//     console.log("good string");
+// }
+
+
+// alert and prompt
+// alert ("SOMETHING IS WRONG");
+
+
+// prompt("please enter your roll number");
+
+let firstName=prompt("enter your first name");
+let lastName=prompt("enter your last name");
+
+let msg="Welcome " + firstName + " " + lastName;
+alert(msg);
