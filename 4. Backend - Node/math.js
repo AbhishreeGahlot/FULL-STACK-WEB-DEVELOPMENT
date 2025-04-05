4659879f13b190a492a200e79ed278e0f5ea46c9
Abhishree Gahlot <117459964+AbhishreeGahlot@users.noreply.github.com>
2025-04-05 22:32:07 +0530

    Add files via upload

---
+++ 4. Backend - Node/math.js
@@ -0,0 +1,13 @@
+// module.exports
+
+const sum = (a,b) => a+b;
+const sub= (a,b) => a-b;
+const mul = (a,b) => a*b;
+
+let abc=
+{
+    sum : sum , 
+    sub : sub , 
+    mul : mul 
+};
+module.exports = abc; //abc is object we made
---
+++ 4. Backend - Node/script.js
@@ -0,0 +1,26 @@
+// cd C:/Users/abhis/OneDrive/Desktop/MERN_STACK/APNA_COLLEGE/5_Backend
+
+
+
+// use of process.argv
+// let args=process.argv;
+
+// for(let i = 2 ;i<args.length ; i++)
+// {
+//     console.log("hello to " , args[i]);
+// }
+//write in bash
+//$ node script.js aman shaman jinu
+ 
+
+
+// module.export
+// write in bash : $ node script.js
+// const math = require("./math");
+// console.log(math); 
+//printing math abc object
+
+
+//exporting directories
+const info = require("./Fruits");
+console.log(info);
