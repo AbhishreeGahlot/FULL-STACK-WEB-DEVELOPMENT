d45de5b3b8a3a1104223db68e900f2e748ea6b11
Abhishree Gahlot <117459964+AbhishreeGahlot@users.noreply.github.com>
2025-06-24 09:56:40 +0530

    Add files via upload

---
+++ 13. React/React_1/App.jsx
@@ -0,0 +1,19 @@
+import Title from "./Title.jsx";
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <Title/>
+    <p>Chai Se Zyada Saala Ketli Garam Hai.</p>
+    <Description/>
+    </>
+  );  
+}
+
+function Description()
+{
+  return <h2> Ameer Aadmi 15 Minute Mein Apne Kapde Badalta Hai... Aadhe Ghante Mein Apna Ghar Badalta Hai... Ek Ghante Mein Apna Dhandha Badalta Hai... Aur Do Ghante Mein Apni Biwi Badalta Hai.</h2>
+}
+
+export default App
---
+++ 13. React/React_1/Title.jsx
@@ -0,0 +1,12 @@
+import "./Title.css";
+function Title()
+{
+    return (
+    <div className="Title">
+    <h1>Kabira Speaking !!</h1>
+    <p>2 * 2 is {2*2}</p> 
+    </div> 
+    );
+}
+
+export default Title;
